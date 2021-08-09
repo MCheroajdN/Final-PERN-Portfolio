828c0543ad7bf9a9f2a8a34c1f2b0a325daa8f8e
MChris173 <mfonchris273@yahoo.com>
2021-08-09 17:48:31 +0100

    connected backend to database PostgreSQL-pgAdmin, server success

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var db = require('./database');
+
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT ||  5000;
 
@@ -13,5 +15,11 @@ app.use(bodyParser.json());
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}!.`);
 });
+
+db.query('SELECT NOW()', (err, res) => {
+    if (err.error)
+      return console.log(err.error);
+    console.log(`PostgreSQL connected: ${res[0].now}.`);
+  });
 
 module.exports = app;
