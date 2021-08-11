e79ce69f8770d2b34d50e4b974729772188b80ac
MChris173 <mfonchris273@yahoo.com>
2021-08-11 21:56:32 +0100

    added weather icon and text descriptn

--- client/src/Weather.js
+++ client/src/Weather.js
@@ -9,8 +9,13 @@ const Weather = (props) => {
 
   return (
     <Row className="weather">
+      <Col sm="12" md={{ size: 4, offset: 4 }}>
+        <h2>{data.name}</h2>
+        <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt="Weather Icon"/>
+        <span>{data.weather[0].main}</span>&nbsp;
+        <span>{Math.floor(data.main.temp)}&deg;F</span>
      
-     <Col ></Col>
+     </Col>
 
      <Table></Table>
       
