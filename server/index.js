1d3eb65a8ea6b403b497c3651638c3579cc60ad1
MChris173 <mfonchris273@yahoo.com>
2021-08-17 00:21:19 +0100

    changed bodyparser to express

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/api/cities', require('./api/cities'));
 app.use('/api/weather', require('./api/weather'));
